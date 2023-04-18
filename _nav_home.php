8a387068236a51eecbee46e506020052c890b9a2
Devendra137 <101552283+Devendra137@users.noreply.github.com>
2023-04-18 19:56:08 +0530

    navigation_home

---
+++ _nav_home.php
@@ -0,0 +1,52 @@
+<!-- Navigation -->
+
+<head>
+    <style>
+        img {
+            border-radius: 50%;
+            margin: auto;
+        }
+    </style>
+
+</head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="logo.jpg" alt="avatar" height="36" style="left-align">
+        </a>
+        <div class="container-fluid" >
+    <a class="navbar-brand">IIT PATNA TRAINING AND PLACEMENT CELL</a>
+    </div>
+    
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"></a>
+                </li>
+    </ul>
+    </div>
+                <!-- <li class="nav-item dropdown"> 
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div> -->
+</nav>
